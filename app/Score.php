6f748968e283c70b78ad95eac3392b240fa89900
vagrant <vagrant@homestead>
2018-01-11 01:42:33 +0000

    Completed changes to posting notes

--- app/Score.php
+++ app/Score.php
@@ -58,13 +58,15 @@ class Score extends Model
         $formula = $this->registration->room->formula_id;
         if ($formula == 2) {
             if ($this->punctuation_d > 0) {
-                return ctof(intval(($this->punctuation_c + $this->punctuation_d) / 2));
+                $average = intval(($this->punctuation_a + $this->punctuation_c) / 2);
+                return ctof(intval(($average + $this->punctuation_d) / 2));
             } else {
                 return ctof(intval(($this->punctuation_a + $this->punctuation_c) / 2));
             }
         } elseif ($formula == 3) {
             if ($this->punctuation_d > 0) {
-                return ctof(intval(($this->punctuation_c + $this->punctuation_d) / 2));
+                $average = intval(($this->punctuation_a + $this->punctuation_b + $this->punctuation_c) / 3);
+                return ctof(intval(($average + $this->punctuation_d) / 2));
             } else {
                 return ctof(intval(($this->punctuation_a + $this->punctuation_b + $this->punctuation_c) / 3));
             }
