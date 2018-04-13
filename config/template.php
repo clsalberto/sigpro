5eb611e8ec544d247611343888f97892e9161a40
Carlos Alberto <clsalberto@outlook.com>
2018-04-13 12:19:04 -0300

    Alter release

--- config/template.php
+++ config/template.php
@@ -11,7 +11,7 @@ return [
     'logo_mini' => '<i class="fa fa-dot-circle-o"></i>',
     'institution' => [
 	    'name' => 'UECE - Pronatec',
-	    'address' => 'Av. Dr. Silas Munguba, 1700 - Campus do Itaperi',
+	    'address' => 'Av. Dr. Silas Munguba, 1700 Campus do Itaperi',
 	    'cnpj' => '07.885.809/0001-97',
         'media' => '6.0',
     ],
