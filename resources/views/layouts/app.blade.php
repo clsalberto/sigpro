06dbbe9d4b5acb912cd8477fae6f7ec02a2a7f56
Carlos Alberto <clsalberto@outlook.com>
2018-04-29 19:35:43 -0300

    Add icons system

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,17 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3c8dbc">
+
+    <meta name="msapplication-TileColor" content="#3c8dbc">
+    <meta name="theme-color" content="#ffffff">
+
     <title>{{ config('app.name') }} - @yield('title')</title>
 
     <!-- Bootstrap -->
